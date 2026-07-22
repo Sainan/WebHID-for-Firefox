83c3550a085fff410edd5a6df5adea8a2ef30464
Sainan <63328889+Sainan@users.noreply.github.com>
2026-07-22 14:25:14 +0200

    Bump version

--- server/main.cpp
+++ server/main.cpp
@@ -365,7 +365,7 @@ fPOzDget78P/d2IgzbaKEA==
 		{
 			s.custom_data.getStructFromMap(ClientData).supports_report_ids = true;
 		}
-		ServerWebService::wsSendText(s, "ver:0.2.3");
+		ServerWebService::wsSendText(s, "ver:0.2.4");
 	};
 	web_srv.on_websocket_message = [](WebSocketMessage& msg, Socket& s, ServerWebService&)
 	{
